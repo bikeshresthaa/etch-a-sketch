0f51acc74b13d8113c1f3fab5a3efcc564648c1c
bikeshresthaa <eshbikesh@gmail.com>
2025-08-03 11:36:39 +0545

    set gridItem size dynamically

--- script.js
+++ script.js
@@ -1,11 +1,41 @@
 const gridContainer = document.createElement("div");
 gridContainer.classList.add("gridContainer");
 
+const btnContainer = document.createElement("div");
+btnContainer.classList.add("buttons");
+
 const body = document.querySelector("body");
 body.appendChild(gridContainer);
+body.appendChild(btnContainer);
 
-//function to create grid
-function createGrid(input){
+const resetBtn = document.createElement("button");
+resetBtn.textContent = "RESET";
+resetBtn.classList.add("button");
+
+const newGrid = document.createElement("button");
+newGrid.textContent = "NEW";
+newGrid.classList.add("button");
+
+btnContainer.appendChild(resetBtn);
+btnContainer.appendChild(newGrid);
+
+createGrid();
+
+
+
+
+
+
+function createPrompt(){
+    let input = prompt("Please Enter Grid Dimension(eg: 16*16, 16 by 16)\nNOTE:- max size is 100*100", "5*5");
+    return input;
+}
+
+
+function createGrid(){
+
+    const input = createPrompt();
+
     let newInput = input.toLowerCase().replace(" ", "");
     let dimensionArray;
 
@@ -17,13 +47,22 @@ function createGrid(input){
         alert("Please Enter a valid input format(eg. 10*10, 10by10)");
     }
     
+    gridContainer.innerHTML= "";
+
+    const itemSize = 600 / dimensionArray[0];
     let gridDimension = dimensionArray[0] * dimensionArray[1];
 
-    for(i=0; i<gridDimension;i++){
+    for(let i=0; i<gridDimension;i++){
         const item = document.createElement("div");
         item.classList.add('grid-item');
-        item.textContent = `item ${i}`;
+        item.style.width = `${itemSize}px`;
+        item.style.height = `${itemSize}px`;
         gridContainer.appendChild(item);
     }
+
+
+    
+
+    
 }
 
